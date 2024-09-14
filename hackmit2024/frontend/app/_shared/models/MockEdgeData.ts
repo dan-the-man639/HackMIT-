af51355dc23498b5b55ee1ab45e81a7ef33a3a8d
LeoLiao031 <114187109+LeoLiao031@users.noreply.github.com>
2024-09-14 17:11:14 -0400

    added nodes

---
+++ hackmit2024/frontend/app/_shared/models/MockEdgeData.ts
@@ -0,0 +1,16 @@
+import { EdgeStruct } from "./Edge";
+
+export const MockEdgeStructData: EdgeStruct[] = [
+    {
+        type: "straight",
+        source: "1",
+        target: "2",
+        id: "edge 1"
+    },
+    {
+        type: "straight",
+        source: "2",
+        target: "3",
+        id: "edge 2"
+    }
+]
