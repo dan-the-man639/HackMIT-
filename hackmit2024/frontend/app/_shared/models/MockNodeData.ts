35cd7fcdac7778fde4837e632a20696778bed935
angelaxudev333 <angela.xu.dev@gmail.com>
2024-09-15 05:26:34 -0400

    laggy 21 nodes

--- hackmit2024/frontend/app/_shared/models/MockNodeData.ts
+++ hackmit2024/frontend/app/_shared/models/MockNodeData.ts
@@ -6,7 +6,7 @@ export const MockNodeStructData: NodeStruct[] = [
     type: "categoryNode",
     position: {
       x: 100,
-      y: 100,
+      y: 800,
     },
     data: {
       imageSource: "Azusawa_Kohane_Cards.png",
@@ -19,8 +19,8 @@ export const MockNodeStructData: NodeStruct[] = [
     id: "2",
     type: "categoryNode",
     position: {
-      x: 600,
-      y: 200,
+      x: 1000,
+      y: 380,
     },
     data: {
       imageSource: "emu_teaparty.jpg",
@@ -33,7 +33,7 @@ export const MockNodeStructData: NodeStruct[] = [
     id: "3",
     type: "categoryNode",
     position: {
-      x: 1100,
+      x: 2000,
       y: 300,
     },
     data: {
@@ -47,8 +47,246 @@ export const MockNodeStructData: NodeStruct[] = [
     id: "4",
     type: "categoryNode",
     position: {
-      x: 1100,
+      x: 2000,
       y: 100,
+    },
+    data: {
+      imageSource: `ena_loli.png`,
+      dialogTitle: "ENAAAAAA SHINOMEEEE",
+      dialogContents: "she is so cute omggomgomo gomgoomgom gmomgomgg",
+      externalSources: [],
+    },
+  },
+  {
+    id: "6",
+    type: "categoryNode",
+    position: {
+      x: 2000,
+      y: 500,
+    },
+    data: {
+      imageSource: `ena_loli.png`,
+      dialogTitle: "ENAAA 1",
+      dialogContents: "she is so cute omggomg   omogomgoo mgomgmomgomgg",
+      externalSources: [],
+    },
+  },
+  {
+    id: "7",
+    type: "categoryNode",
+    position: {
+      x: 2000,
+      y: 700,
+    },
+    data: {
+      imageSource: `ena_loli.png`,
+      dialogTitle: "ENAAAAAA SHINOMEEEE",
+      dialogContents: "she is so cute omggomgomo gomgoomgom gmomgomgg",
+      externalSources: [],
+    },
+  },
+  {
+    id: "8",
+    type: "categoryNode",
+    position: {
+      x: 1000,
+      y: 1300,
+    },
+    data: {
+      imageSource: "emu_teaparty.jpg",
+      dialogTitle: "EMU CUTIE",
+      dialogContents: "she is so cute omggomgomogomg oomgomgmomgomgg",
+      externalSources: [],
+    },
+  },
+  {
+    id: "9",
+    type: "categoryNode",
+    position: {
+      x: 2000,
+      y: 1000,
+    },
+    data: {
+      imageSource: `ena_loli.png`,
+      dialogTitle: "ENAAA 1",
+      dialogContents: "she is so cute omggomg   omogomgoo mgomgmomgomgg",
+      externalSources: [],
+    },
+  },
+  {
+    id: "10",
+    type: "categoryNode",
+    position: {
+      x: 2000,
+      y: 1200,
+    },
+    data: {
+      imageSource: `ena_loli.png`,
+      dialogTitle: "ENAAAAAA SHINOMEEEE",
+      dialogContents: "she is so cute omggomgomo gomgoomgom gmomgomgg",
+      externalSources: [],
+    },
+  },
+  {
+    id: "11",
+    type: "categoryNode",
+    position: {
+      x: 2000,
+      y: 1400,
+    },
+    data: {
+      imageSource: `ena_loli.png`,
+      dialogTitle: "ENAAA 1",
+      dialogContents: "she is so cute omggomg   omogomgoo mgomgmomgomgg",
+      externalSources: [],
+    },
+  },
+  {
+    id: "12",
+    type: "categoryNode",
+    position: {
+      x: 2000,
+      y: 1600,
+    },
+    data: {
+      imageSource: `ena_loli.png`,
+      dialogTitle: "ENAAAAAA SHINOMEEEE",
+      dialogContents: "she is so cute omggomgomo gomgoomgom gmomgomgg",
+      externalSources: [],
+    },
+  },
+  {
+    id: "13",
+    type: "categoryNode",
+    position: {
+      x: 1000,
+      y: 2220,
+    },
+    data: {
+      imageSource: "emu_teaparty.jpg",
+      dialogTitle: "EMU CUTIE",
+      dialogContents: "she is so cute omggomgomogomg oomgomgmomgomgg",
+      externalSources: [],
+    },
+  },
+  {
+    id: "14",
+    type: "categoryNode",
+    position: {
+      x: 1000,
+      y: 3140,
+    },
+    data: {
+      imageSource: `ena_loli.png`,
+      dialogTitle: "ENAAA 1",
+      dialogContents: "she is so cute omggomg   omogomgoo mgomgmomgomgg",
+      externalSources: [],
+    },
+  },
+  {
+    id: "15",
+    type: "categoryNode",
+    position: {
+      x: 2000,
+      y: 1900,
+    },
+    data: {
+      imageSource: `ena_loli.png`,
+      dialogTitle: "ENAAAAAA SHINOMEEEE",
+      dialogContents: "she is so cute omggomgomo gomgoomgom gmomgomgg",
+      externalSources: [],
+    },
+  },
+  {
+    id: "16",
+    type: "categoryNode",
+    position: {
+      x: 2000,
+      y: 2100,
+    },
+    data: {
+      imageSource: `ena_loli.png`,
+      dialogTitle: "ENAAA 1",
+      dialogContents: "she is so cute omggomg   omogomgoo mgomgmomgomgg",
+      externalSources: [],
+    },
+  },
+  {
+    id: "17",
+    type: "categoryNode",
+    position: {
+      x: 2000,
+      y: 2300,
+    },
+    data: {
+      imageSource: `ena_loli.png`,
+      dialogTitle: "ENAAAAAA SHINOMEEEE",
+      dialogContents: "she is so cute omggomgomo gomgoomgom gmomgomgg",
+      externalSources: [],
+    },
+  },
+  {
+    id: "18",
+    type: "categoryNode",
+    position: {
+      x: 2000,
+      y: 2500,
+    },
+    data: {
+      imageSource: "emu_teaparty.jpg",
+      dialogTitle: "EMU CUTIE",
+      dialogContents: "she is so cute omggomgomogomg oomgomgmomgomgg",
+      externalSources: [],
+    },
+  },
+  {
+    id: "19",
+    type: "categoryNode",
+    position: {
+      x: 2000,
+      y: 2800,
+    },
+    data: {
+      imageSource: `ena_loli.png`,
+      dialogTitle: "ENAAA 1",
+      dialogContents: "she is so cute omggomg   omogomgoo mgomgmomgomgg",
+      externalSources: [],
+    },
+  },
+  {
+    id: "20",
+    type: "categoryNode",
+    position: {
+      x: 2000,
+      y: 3000,
+    },
+    data: {
+      imageSource: `ena_loli.png`,
+      dialogTitle: "ENAAAAAA SHINOMEEEE",
+      dialogContents: "she is so cute omggomgomo gomgoomgom gmomgomgg",
+      externalSources: [],
+    },
+  },
+  {
+    id: "21",
+    type: "categoryNode",
+    position: {
+      x: 2000,
+      y: 3200,
+    },
+    data: {
+      imageSource: `ena_loli.png`,
+      dialogTitle: "ENAAA 1",
+      dialogContents: "she is so cute omggomg   omogomgoo mgomgmomgomgg",
+      externalSources: [],
+    },
+  },
+  {
+    id: "22",
+    type: "categoryNode",
+    position: {
+      x: 2000,
+      y: 3400,
     },
     data: {
       imageSource: `ena_loli.png`,
