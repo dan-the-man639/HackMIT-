4bd51bd274c9254c3c43364dcfc738887fecbc33
dan-the-man639 <108316903+dan-the-man639@users.noreply.github.com>
2024-09-15 10:38:59 -0400

    cal update

--- hackmit2024/frontend/app/_shared/models/MockCalcNodes.tsx
+++ hackmit2024/frontend/app/_shared/models/MockCalcNodes.tsx
@@ -9,7 +9,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 800,
     },
     data: {
-      imageSource: "/calculas/0.jpg",
+      imageSource: "/calculas/lol.jpg",
       dialogTitle: "teach me about calculus",
       dialogContents: "Calculus is a branch of mathematics that deals with rates of change and accumulation. It is divided into two main branches: differential calculus, which focuses on the study of rates of change and slopes of graphs, and integral calculus, which relates to the accumulation of quantities and the calculation of areas under curves. By understanding calculus, we can solve various real-world problems in physics, engineering, economics, and other fields by analyzing and predicting how quantities change and accumulate over time. Calculus is a fundamental tool for gaining deeper insight into the workings of the natural world.",
       externalSources: ["https://en.wikipedia.org/wiki/Calculus"],
@@ -23,7 +23,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 380,
     },
     data: {
-      imageSource: "/calculas/1.png",
+      imageSource: "/calculas/0.png",
       dialogTitle: "Introduction to Calculus",
       dialogContents: "Calculus is a branch of mathematics that deals with the study of rates of change and accumulation. It is divided into two main branches: differential calculus, which involves the study of instantaneous rates of change, and integral calculus, which deals with the accumulation of quantities over a period of time. Calculus is essential in various fields such as physics, engineering, economics, and computer science, as it provides a precise way to analyze and model complex systems. By understanding calculus, one can solve a wide range of problems and make more accurate predictions in real-world scenarios.",
       externalSources: ["https://en.wikipedia.org/wiki/Introduction_to_Calculus"],
@@ -37,7 +37,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 300,
     },
     data: {
-      imageSource: `/calculas/2.png`,
+      imageSource: `/calculas/1.png`,
       dialogTitle: "Differentiation",
       dialogContents: "Differentiation is a fundamental concept in calculus that involves finding the rate at which a function is changing. It allows us to determine the slope of a curve at a specific point, and can be used to solve various real-world problems such as maximizing or minimizing functions. By understanding differentiation, we can analyze the behavior of functions and make predictions about their future values. This skill is crucial in fields such as physics, engineering, economics, and many others where it is necessary to interpret and manipulate mathematical models.",
       externalSources: ["https://en.wikipedia.org/wiki/Differentiation"],
@@ -51,7 +51,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 100,
     },
     data: {
-      imageSource: `/calculas/3.jpg`,
+      imageSource: `/calculas/2.jpg`,
       dialogTitle: "Integration",
       dialogContents: "Integration is a key concept in calculus that involves finding the area under a curve or the accumulation of a quantity over a certain interval. It is the reverse operation of differentiation, and both concepts are fundamental to understanding calculus. Integration allows us to find the total change in a function or the total amount of a quantity. It is used in various fields such as physics, engineering, economics, and more to solve complex problems and analyze patterns. Understanding integration is crucial for mastering calculus and its applications in real-world situations.",
       externalSources: ["https://en.wikipedia.org/wiki/Integration"],
@@ -65,7 +65,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 500,
     },
     data: {
-      imageSource: `/calculas/4.jpg`,
+      imageSource: `/calculas/3.jpg`,
       dialogTitle: "Applications of Calculus",
       dialogContents: "Calculus is used in a wide range of fields such as physics, engineering, economics, and computer science to solve complex problems and model real-world situations. It is particularly useful for analyzing rates of change, maximizing or minimizing functions, and understanding the behavior of systems over time. Calculus is also essential for various scientific and technological advancements, such as designing efficient structures, developing algorithms for data analysis, and predicting the behavior of financial markets. Understanding calculus can greatly enhance one's problem-solving skills and open up opportunities for innovation and advanced research.",
       externalSources: ["https://en.wikipedia.org/wiki/Calculus#Applications"],
@@ -79,7 +79,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 700,
     },
     data: {
-      imageSource: `/calculas/5.png`,
+      imageSource: `/calculas/4.png`,
       dialogTitle: "Fundamental concepts in calculus",
       dialogContents: "include limits, derivatives, and integrals. Limits are used to understand the behavior of a function as it approaches a certain value. Derivatives measure the rate of change of a function at a given point, while integrals calculate the area under a curve. These concepts are essential for understanding how functions behave and for solving a variety of real-world problems in mathematics, science, and engineering. Calculus provides a powerful tool for analyzing and modeling continuous processes and is foundational for further studies in fields such as physics, economics, and engineering.",
       externalSources: ["https://en.wikipedia.org/wiki/Fundamental_theorem_of_calculus"],
@@ -93,7 +93,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 1300,
     },
     data: {
-      imageSource: "/calculas/6.png",
+      imageSource: "/calculas/5.png",
       dialogTitle: "Differentiation and its applications",
       dialogContents: "Calculus is a branch of mathematics that deals with rates of change and accumulation. One key concept in calculus is differentiation, which is the process of finding the rate at which a quantity is changing at a specific point. This is used in various real-world applications, such as determining the speed of a moving object, finding maximum and minimum values in a function, and analyzing trends in data. Mastering differentiation is essential for understanding and solving complex problems in areas such as physics, engineering, economics, and biology.",
       externalSources: ["https://en.wikipedia.org/wiki/Differentiation"],
@@ -107,7 +107,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 1000,
     },
     data: {
-      imageSource: `/calculas/7.png`,
+      imageSource: `/calculas/6.png`,
       dialogTitle: "Integration and its applications",
       dialogContents: "Integration is a fundamental concept in calculus that involves finding the area under a curve. It is the reverse process of differentiation, where we find the slope of a curve. Integrals are used in a wide range of applications, such as calculating volumes, work done by a force, and probability distributions. Understanding integration is crucial in solving real-world problems in physics, engineering, economics, and many other fields. By learning about integration and its applications, one can gain a deeper understanding of how calculus is used to model and solve complex mathematical problems.",
       externalSources: ["https://en.wikipedia.org/wiki/Integration"],
@@ -121,7 +121,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 1200,
     },
     data: {
-      imageSource: `/calculas/8.jpg`,
+      imageSource: `/calculas/7.jpg`,
       dialogTitle: "Advanced topics in calculus",
       dialogContents: "include topics such as multi-variable calculus, vector calculus, and differential equations. Multi-variable calculus deals with functions of multiple variables and their derivatives, while vector calculus focuses on vectors and vector-valued functions. Differential equations involve studying equations involving derivatives and their solutions. These topics are essential for understanding more complex mathematical and scientific concepts, such as those found in engineering, physics, and economics. Studying advanced topics in calculus can provide a deeper understanding of how functions behave in various dimensions and help in solving real-world problems",
       externalSources: ["https://en.wikipedia.org/wiki/Advanced_calculus"],
@@ -135,7 +135,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 1400,
     },
     data: {
-      imageSource: `/calculas/9.jpg`,
+      imageSource: `/calculas/8.jpg`,
       dialogTitle: "Understanding the concept of differentiation",
       dialogContents: "is essential in calculus. Differentiation is the process of finding the rate at which a function is changing at a specific point. This is crucial in calculating slopes, areas under curves, and maximizing/minimizing functions. It allows us to analyze the behavior of functions and determine critical points. Differentiation is a fundamental concept in calculus that forms the basis for more advanced topics such as integration and optimization. Mastering this concept is key to success in calculus and various fields of science and engineering.",
       externalSources: ["https://en.wikipedia.org/wiki/Differentiation"],
@@ -149,7 +149,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 1600,
     },
     data: {
-      imageSource: `/calculas/10.jpg`,
+      imageSource: `/calculas/9.jpg`,
       dialogTitle: "The importance of differentiation in various fields",
       dialogContents: "Differentiation in calculus is crucial for analyzing rates of change and identifying optimal solutions in fields like engineering, economics, and physics. It allows us to calculate gradients, find maximum and minimum points, and understand how variables interact with each other. By understanding differentiation, we can better model real-world situations and make informed decisions. This fundamental concept is the backbone of calculus and is key to solving complex problems in a wide range of disciplines.",
       externalSources: ["https://en.wikipedia.org/wiki/Differentiation_(economics)"],
@@ -163,7 +163,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 2220,
     },
     data: {
-      imageSource: "/calculas/11.jpg",
+      imageSource: "/calculas/10.jpg",
       dialogTitle: "Strategies for implementing differentiation",
       dialogContents: "One important concept in calculus is differentiation, which is the process of finding the rate at which a function is changing at a specific point. Strategies for implementing differentiation include using the power rule, product rule, quotient rule, and chain rule to find the derivative of a function. These rules allow us to efficiently find the slope of a function, which can help us analyze its behavior and make predictions about its future values. Understanding these differentiation strategies is essential for mastering calculus and solving complex mathematical problems.",
       externalSources: ["https://www.readingrockets.org/article/strategies-differentiated-instruction"],
@@ -177,7 +177,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 3140,
     },
     data: {
-      imageSource: `/calculas/12.jpg`,
+      imageSource: `/calculas/11.jpg`,
       dialogTitle: "Evaluating the effectiveness of differentiation techniques",
       dialogContents: "Differentiation techniques in calculus are used to evaluate the rate of change of a function. By applying these techniques, we can determine the slope of a curve at any given point or find the maximum and minimum values of a function. Understanding and mastering differentiation allows us to solve various real-world problems related to optimization, such as maximizing profits or minimizing costs. It is an essential concept in calculus that forms the foundation for more advanced topics like integration. Mastering differentiation techniques can greatly enhance one's problem-solving skills and analytical thinking in mathematics.",
       externalSources: ["https://files.eric.ed.gov/fulltext/EJ1203695.pdf"],
@@ -191,7 +191,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 1900,
     },
     data: {
-      imageSource: `/calculas/13.jpg`,
+      imageSource: `/calculas/12.jpg`,
       dialogTitle: "Introduction to Integration",
       dialogContents: "Integration is a fundamental concept in calculus that involves finding the total of a quantity (such as area, volume, or displacement) by accumulating infinitesimally small values. This process is the reverse of differentiation, which allows us to find the rate of change of a quantity. Integration helps in solving a variety of real-world problems, including computing areas under curves, determining volumes of irregular shapes, and finding the average value of a function. By learning about integration, one can gain a deeper understanding of calculus and its applications in various fields such as physics, engineering, and economics.",
       externalSources: ["https://en.wikipedia.org/wiki/Introduction_to_integration"],
@@ -205,7 +205,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 2100,
     },
     data: {
-      imageSource: `/calculas/14.jpg`,
+      imageSource: `/calculas/13.jpg`,
       dialogTitle: "Methods of Integration",
       dialogContents: "Methods of integration in calculus are used to find the integral of a function. Some common methods include integration by substitution, integration by parts, and trigonometric substitution. These methods help to simplify complex integrals and make them easier to solve. Each method has its own set of rules and guidelines to follow, and understanding when to apply each method is key in effectively solving integration problems. By learning and practicing these methods, one can become more proficient in solving a wide range of integration problems in calculus.",
       externalSources: ["https://en.wikipedia.org/wiki/Methods_of_integration"],
@@ -219,7 +219,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 2300,
     },
     data: {
-      imageSource: `/calculas/15.jpg`,
+      imageSource: `/calculas/14.jpg`,
       dialogTitle: "Applications of Integration",
       dialogContents: "One of the key concepts in calculus is integration, which involves finding the cumulative area under a curve. This mathematical tool has a wide range of applications in fields such as physics, engineering, economics, and biology. For example, integration can be used to determine the total distance traveled by an object moving with varying speeds, calculate the volume of irregular shapes in three-dimensional space, find the total revenue or profit from a given business model, and analyze population growth or decay rates. Understanding how to apply integration allows us to solve real-world problems and make informed decisions based on mathematical reasoning.",
       externalSources: ["https://en.wikipedia.org/wiki/Applications_of_integration"],
@@ -233,7 +233,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 2500,
     },
     data: {
-      imageSource: "/calculas/16.jpg",
+      imageSource: "/calculas/15.jpg",
       dialogTitle: "Challenges and Common Mistakes in Integration",
       dialogContents: "One common challenge in integration is properly choosing the correct method or approach to solve a given problem. This includes knowing when to use substitution, integration by parts, or other techniques. Another common mistake is not paying close attention to the limits of integration or forgetting to include a constant of integration. It is important to practice and familiarize oneself with various integration techniques to overcome these challenges and avoid these common mistakes when solving calculus problems.",
       externalSources: ["https://www.codingninjas.com/codestudio/library/challenges-and-common-mistakes-in-integration"],
@@ -247,7 +247,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 2800,
     },
     data: {
-      imageSource: `/calculas/17.jpg`,
+      imageSource: `/calculas/16.jpg`,
       dialogTitle: "Physics",
       dialogContents: "Calculus is a branch of mathematics that deals with rates of change and accumulation. In physics, calculus is used to analyze motion, forces, and energy. By studying derivatives and integrals, physicists can describe how an object's position, velocity, and acceleration change over time. Calculus is also essential in solving equations for complex systems and predicting future behavior through mathematical modeling. Understanding calculus allows physicists to accurately describe and predict the behavior of objects and systems in the physical world.",
       externalSources: ["https://en.wikipedia.org/wiki/Physics"],
@@ -261,7 +261,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 3000,
     },
     data: {
-      imageSource: `/calculas/18.jpg`,
+      imageSource: `/calculas/17.jpg`,
       dialogTitle: "Engineering",
       dialogContents: "Calculus is a fundamental mathematical tool used in engineering to model and analyze systems that change continuously. By studying calculus, engineers can determine rates of change, optimize designs, and predict the behavior of complex systems. Engineering applications of calculus include designing structures, controlling processes, and analyzing physical phenomena such as fluid flow and heat transfer. Understanding calculus allows engineers to solve real-world problems and make informed decisions when designing and improving technological systems. Additionally, calculus is essential for understanding advanced topics in engineering such as control theory, signal processing, and electromagnetics.",
       externalSources: ["https://en.wikipedia.org/wiki/Engineering"],
@@ -275,7 +275,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 3200,
     },
     data: {
-      imageSource: `/calculas/19.jpg`,
+      imageSource: `/calculas/18.jpg`,
       dialogTitle: "Economics",
       dialogContents: "Calculus is a branch of mathematics that is essential for understanding economic concepts such as optimization, marginal analysis, and elasticity. By studying calculus, you can analyze how changes in variables affect economic outcomes, make more informed decisions in areas such as production and pricing, and comprehend theories like supply and demand. Calculus also helps in modeling complicated economic systems and predicting future trends, making it a valuable tool for economists who want to understand the underlying mathematical relationships in economic phenomena.",
       externalSources: ["https://en.wikipedia.org/wiki/Economics"],
@@ -289,7 +289,7 @@ export const CalcNodes: NodeStruct[] = [
       y: 3400,
     },
     data: {
-      imageSource: `/biology/bio1.png`,
+      imageSource: `/biology/19.png`,
       dialogTitle: "Biology",
       dialogContents: "Calculus is not directly related to the field of biology, but it can still be useful in analyzing biological processes. Calculus can be used to study rates of change, such as the growth of populations or the change in concentration of a substance in a biological system. By understanding calculus, biologists can make more accurate predictions and models of biological phenomena, leading to a deeper understanding of how living organisms function and interact with their environment.",
       externalSources: ["https://en.wikipedia.org/wiki/Applications_of_calculus"],
